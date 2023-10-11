b293508c3352f1e65eaf7c8ba606360aa2fa72bc
Enyu Zhao <469070269@qq.com>
2023-10-11 00:09:01 -0700

    modified agent file

--- agent.py
+++ agent.py
@@ -15,25 +15,40 @@ class Agent:
     def __init__(self, agent_id, args):
         self.args = args
         self.agent_id = agent_id
-        if getattr(self.args,"algorithm","FACMAC") == "FACMAC":
-            self.policy=FACMAC(args,agent_id)
-        elif getattr(self.args,"algorithm","FACMAC") == "MADDPG":
-            self.policy = MADDPG(args, agent_id)
-        elif getattr(self.args,"algorithm","FACMAC") == "IDDPG":
-            self.policy = IDDPG(args, agent_id)
-        elif getattr(self.args,"algorithm","FACMAC") == "MADDPGLSTMactor":
-            self.policy = MADDPG_lstm_actor(args, agent_id)
-        elif getattr(self.args,"algorithm","FACMAC") == "FACMACLSTMactor":
-            self.policy = FACMAC_lstm_actor(args, agent_id)
-        elif getattr(self.args,"algorithm","FACMAC") == "FAC":
-            self.policy = FAC(args, agent_id)
-        elif getattr(self.args,"algorithm","FACMAC") == "FACMAC_SCH":
-            self.policy = FACMACSCH(args, agent_id)
-        elif getattr(self.args,"algorithm","FACMAC") == "FACLSTM":
-            self.policy = FACLSTM(args, agent_id)
+        policy_name=getattr(self.args,"algorithms","MADDPG")
+        self.policy_class={
+            "FACMAC":{"policy":FACMAC(args,agent_id),"sep_action":True},
+            "MADDPG":{"policy":MADDPG(args,agent_id),"sep_action":False},
+            "IDDPG":{"policy":IDDPG(args,agent_id),"sep_action":True},
+            "MADDPGLSTMactor":{"policy":MADDPG_lstm_actor(agent_id),"sep_action":False},
+            "FAC":{"policy":FAC(args,agent_id),"sep_action":True},
+            "FACMAC_SCH":{"policy":FACMACSCH(args,agent_id),"sep_action":True},
+            "MADDPGLSTM":{"policy":MADDPGLSTM(args,agent_id), "sep_action":False},           
+        }
+        
+        self.policy=self.policy_class[policy_name]["policy"]
+        self.sep_action=self.policy_class[policy_name]["sep_action"]
+        
+        
+        # if getattr(self.args,"algorithm","FACMAC") == "FACMAC":
+        #     self.policy=FACMAC(args,agent_id)
+        # elif getattr(self.args,"algorithm","FACMAC") == "MADDPG":
+        #     self.policy = MADDPG(args, agent_id)
+        # elif getattr(self.args,"algorithm","FACMAC") == "IDDPG":
+        #     self.policy = IDDPG(args, agent_id)
+        # elif getattr(self.args,"algorithm","FACMAC") == "MADDPGLSTMactor":
+        #     self.policy = MADDPG_lstm_actor(args, agent_id)
+        # elif getattr(self.args,"algorithm","FACMAC") == "FACMACLSTMactor":
+        #     self.policy = FACMAC_lstm_actor(args, agent_id)
+        # elif getattr(self.args,"algorithm","FACMAC") == "FAC":
+        #     self.policy = FAC(args, agent_id)
+        # elif getattr(self.args,"algorithm","FACMAC") == "FACMAC_SCH":
+        #     self.policy = FACMACSCH(args, agent_id)
+        # elif getattr(self.args,"algorithm","FACMAC") == "FACLSTM":
+        #     self.policy = FACLSTM(args, agent_id)
 
-        elif getattr(self.args,"algorithm","FACMAC") == "MADDPGLSTM":
-            self.policy = MADDPGLSTM(args, agent_id)
+        # elif getattr(self.args,"algorithm","FACMAC") == "MADDPGLSTM":
+        #     self.policy = MADDPGLSTM(args, agent_id)
 
     def select_action(self, o, noise_rate, epsilon):
         device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
@@ -43,15 +58,17 @@ class Agent:
 
         else:
             inputs = torch.tensor(o, dtype=torch.float32).unsqueeze(0).to(device)
-            # Do something here pls this is so ugly
             
-            if getattr(self.args, "algorithm", "FACMAC") == "FACMAC" or getattr(self.args, "algorithm", "FACMAC") == "IDDPG" or getattr(self.args, "algorithm", "FACMAC") == "FAC" or getattr(self.args,"algorithm","FACMAC") == "FACMAC_SCH":
+            
+            # if getattr(self.args, "algorithm", "FACMAC") == "FACMAC" or getattr(self.args, "algorithm", "FACMAC") == "IDDPG" or getattr(self.args, "algorithm", "FACMAC") == "FAC" or getattr(self.args,"algorithm","FACMAC") == "FACMAC_SCH":
+            if self.sep_action:
                 pi = self.policy.actor_network(inputs)["actions"].squeeze(0)
 
-            elif getattr(self.args, "algorithm", "FACMAC") == "MADDPG"or getattr(self.args, "algorithm", "FACMAC") == "MADDPGLSTMactor" or getattr(self.args, "algorithm", "FACMAC") == "FACMACLSTMactor" or getattr(self.args,"algorithm","FACMAC") == "FACLSTM" or getattr(self.args,"algorithm","FACMAC") == "MADDPGLSTM":
+            # elif getattr(self.args, "algorithm", "FACMAC") == "MADDPG"or getattr(self.args, "algorithm", "FACMAC") == "MADDPGLSTMactor" or getattr(self.args, "algorithm", "FACMAC") == "FACMACLSTMactor" or getattr(self.args,"algorithm","FACMAC") == "FACLSTM" or getattr(self.args,"algorithm","FACMAC") == "MADDPGLSTM":
+            else:
                 pi = self.policy.actor_network(inputs).squeeze(0)
 
-            # print('{} : {}'.format(self.name, pi))
+            
             u = pi.cpu().numpy()
 
             noise = noise_rate * self.args.high_action * np.random.randn(*u.shape)  # gaussian noise
